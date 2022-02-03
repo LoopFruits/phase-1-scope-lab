5be0a3e03e2fe92c5998e940b1c4e198037fa3b4
LoopFruits <Kensworldc@gmail.com>
2022-02-03 11:44:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // Write your solution in this file!
 var customerName = 'bob';
-console.log('num3:', customerName); // We're doing this to see what customername is returning
+// console.log('num3:', customerName); // We're doing this to see what customername is returning
 //num3 : bob
 function upperCaseCustomerName(){
     customerName = customerName.toUpperCase(); // defining customername to become uppercase
-    console.log('num6:' , customerName);
+    // console.log('num6:' , customerName);
     // num6 : BOB
 }
  upperCaseCustomerName();
-console.log('num9: ', customerName);
-// after node index.js we get => num9 : BOB
+// console.log('num9: ', customerName);
+// after node index.js we get => num9 : BOB // looking at the variable we execute in the real time. by doing node index.js 
+
+
+//with var their both the same thing. because we use var, and the variable being referenced on line 5 is the same as line 2 due to var. its operating on the original. invoking the function changed the uppercase before we can invoke(or execute)
+
